fe3c21f578679dcf8f85de3b8a5357a7ecce5116
Ali Asghar <taghizade12@gmail.com>
2017-01-16 11:06:26 +0330

    added some views

--- Book/views.py
+++ Book/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 
 
 def home(request):
+    page = loader.get_template('index.html')
+    return HttpResponse(page.render())
+
+
+def about(request):
     page = loader.get_template('about.html')
+    return HttpResponse(page.render())
+
+
+def basket(request):
+    page = loader.get_template('basket.html')
+    return HttpResponse(page.render())
+
+
+def contact(request):
+    page = loader.get_template('contact.html')
     return HttpResponse(page.render())
