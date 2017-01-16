c70d08c2f36fd2eb7e0900f9092d656341d111d9
Ali Asghar <taghizade12@gmail.com>
2017-01-16 20:38:03 +0330

    added category

--- Book/urls.py
+++ Book/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^book/(\d+)$', views.book_detail, name='book_detail'),
     url(r'^about$', views.about, name='about'),
     url(r'^contact$', views.contact, name='contact'),
+    url(r'^category/(\d+)', views.category, name='category')
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
