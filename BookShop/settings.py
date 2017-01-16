dd363835f79de7a11ae3f452b9fdc512d1a8dcda
AnonymousHH <ich.bin1373@yahoo.com>
2017-01-16 11:09:37 +0330

    correct page style

--- BookShop/settings.py
+++ BookShop/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'BookShop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'Book/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
